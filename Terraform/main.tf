3c02cf9a04ff3990405da91b4fa79a568d01c2a3
hopzygirl <ookaliemeroo@gmail.com>
2025-11-07 16:02:43 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,22 @@
+#====================================================================
+# Configuration Provider AWS
+#====================================================================
+
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    aws = {
+      source = "hshicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+#Configuration AWS
 provider "aws" {
   region = var.region
   access_key = var.access_key
   secret_key = var.secret_key
 }
+
