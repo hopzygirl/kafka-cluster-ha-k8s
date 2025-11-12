3a7b7c5262f5073f3f3274a08dae1d84656e2ea4
hopzygirl <ookaliemeroo@gmail.com>
2025-11-12 16:11:49 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,8 @@ terraform {
 # Configuration AWS - utilise vos credentials
 provider "aws" {
   region = var.aws_region
-  
+  access_key = var.access_key
+  secret_key = var.secret_key
   default_tags {
     tags = {
       Project     = var.project_name
